de424ddb023695fe3a10a45fedf3cdd525d56593
Mike de Groot - Remote <mike@entwurf.co.uk>
2022-01-15 12:20:16 +0000

    async code set up

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ export const duplicateValue = (a) => {
   const b = a;
   return b;
 };
+
+const mockApi = async () => {
+  let time = 0;
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      time = 10;
+      resolve();
+    }, 3000)
+  );
+  return time;
+};
+
+export const fetchData = async () => {
+  console.log("1");
+  try {
+    const data = await mockApi();
+    console.log("2");
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchData();
